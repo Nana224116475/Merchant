d5d592d6965eea4199a6cde4987c63a55bcd6a63
Don Nana <nana15@validusprep.org>
2013-07-26 11:20:21 -0400

    Finish with I1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Merchant::Application.config.secret_key_base = '431f7de29e1251dbdd01983a4102437cd711662756dfee80ff1de7f6809ffb1f4376b8dd2a520d1ccd7f5f4e92cf8181bf3b696d4a1e621b2f86bd953b8dad69'
